db6781bbfcf3f04fcec5fa6cc2b362914b691c5c
tanyaweaver <tanya83@uw.edu>
2016-08-28 21:53:42 -0700

    done, tests are passing

---
+++ src/test_sort_cards.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf -8 -*-
+import pytest
+from sort_cards import sort_cards
+
+TEST = [
+    ((list('39A5T824Q7J6K')), list('A23456789TJQK')),
+    ((list('Q286JK395A47T')), list('A23456789TJQK')),
+    ((list('54TQKJ69327A8')), list('A23456789TJQK')),
+]
+
+
+@pytest.mark.parametrize('shuffled, sorted', TEST)
+def test_sort_cards(shuffled, sorted):
+    """
+    Tests whether shuffled cards are sorted properly by sort_cards().
+    """
+    assert sort_cards(shuffled) == sorted
+
+
+def test_sort_error():
+    """
+    Test whether a KeyError is raised when there is not a card
+    in the list.
+    """
+    with pytest.raises(KeyError):
+        sort_cards(list('ABCD12345'))
