6035003dc848b2fe6befb67666afa6a7886a2bd6
tanyaweaver <tanya83@uw.edu>
2017-06-04 17:14:34 -0700

    reverse linked list in groups of k elements

---
+++ test/test_reverse_k_llist.py
@@ -0,0 +1,31 @@
+import pytest
+from src.reverse_k_llist import Llist
+
+
+LIST = [
+    ([1, 2, 3, 4], [1, 2, 3, 4]),
+    ([], []),
+    (None, [])
+]
+
+TEST = [
+    ([1, 2, 3, 4, 5, 6, 7, 8], 3, [3, 2, 1, 6, 5, 4, 8, 7]),
+    ([], 5, []),
+    ([1, 2, 3], 4, [3, 2, 1]),
+    ([1], 1, [1]),
+    ([1, 2], 1, [1, 2]),
+    ([1, 2, 3, 4], 2, [2, 1, 4, 3])
+]
+
+
+@pytest.mark.parametrize('l, result', LIST)
+def test_list_add_print(l, result):
+    llist = Llist(l)
+    assert llist.print_list() == result
+
+
+@pytest.mark.parametrize('l, k, result', TEST)
+def test_reverse_k1(l, k, result):
+    llist = Llist(l)
+    llist.reverse_k_elements(k)
+    assert llist.print_list() == result
